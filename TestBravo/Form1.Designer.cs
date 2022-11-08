de7dd16534f10b5212ca79db05b2d5a2c4c5e8f4
Bình Thắng Võ <vobinhthang123@gmail.com>
2022-11-08 16:24:32 +0700

    Add project files.

---
+++ TestBravo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TestBravo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gvCustomer = new System.Windows.Forms.DataGridView();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lbPageNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvCustomer
+            // 
+            this.gvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvCustomer.Location = new System.Drawing.Point(12, 12);
+            this.gvCustomer.Name = "gvCustomer";
+            this.gvCustomer.RowHeadersWidth = 51;
+            this.gvCustomer.RowTemplate.Height = 24;
+            this.gvCustomer.Size = new System.Drawing.Size(726, 389);
+            this.gvCustomer.TabIndex = 0;
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(177, 441);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(85, 39);
+            this.btnPre.TabIndex = 1;
+            this.btnPre.Text = "<";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(424, 441);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(85, 39);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lbPageNumber
+            // 
+            this.lbPageNumber.AutoSize = true;
+            this.lbPageNumber.Location = new System.Drawing.Point(319, 452);
+            this.lbPageNumber.Name = "lbPageNumber";
+            this.lbPageNumber.Size = new System.Drawing.Size(44, 16);
+            this.lbPageNumber.TabIndex = 3;
+            this.lbPageNumber.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 529);
+            this.Controls.Add(this.lbPageNumber);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.gvCustomer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvCustomer;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lbPageNumber;
+    }
+}
+
---
+++ TestBravo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestBravo
+{
+    public partial class Form1 : Form
+    {
+
+        int pageNumber = 1;
+        int rowOfPage = 50;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            int totalRecord = 0;
+            using (TestBravoEntities db = new TestBravoEntities())
+            {
+                totalRecord = db.tbl_Customer.Count();
+            };
+
+            
+            lbPageNumber.Text = string.Format("Page {0}/{1}", pageNumber, totalRecord / rowOfPage);
+
+        }
+        
+        private async Task LoadDataCustomer(int pageNumber, int rowOfPage)
+        {
+           
+            string connStr = "Data Source=THANG-PC\\SQLEXPRESS; Initial Catalog=TestBravo; User ID=sa;Password=sa123";
+            string query = @"DECLARE @PageNumber AS INT
+                            DECLARE @RowsOfPage AS INT
+                            SET @PageNumber="+pageNumber+"SET @RowsOfPage="+rowOfPage+"SELECT * FROM [tbl_Customer]ORDER BY Id OFFSET (@PageNumber-1)*@RowsOfPage ROWS FETCH NEXT @RowsOfPage ROWS ONLY";
+
+
+            SqlConnection conn = new SqlConnection(connStr);
+            
+               conn.Open();
+
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+
+                DataTable data = new DataTable();
+                
+                SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+                if (reader.HasRows)
+                {
+                    data.Load(reader);
+                
+                }
+                
+                conn.Close();
+            gvCustomer.DataSource = data;
+        }
+
+
+        private async void btnNext_Click(object sender, EventArgs e)
+        {
+            int totalRecord = 0;
+            using (TestBravoEntities db = new TestBravoEntities())
+            {
+                totalRecord = db.tbl_Customer.Count();
+            };
+            if (pageNumber < totalRecord / rowOfPage)
+            {
+                
+                pageNumber++;
+                await LoadDataCustomer(pageNumber, rowOfPage);
+                lbPageNumber.Text = string.Format("Page {0}/{1}", pageNumber, totalRecord / rowOfPage);
+
+            }
+        }
+
+        private async void btnPre_Click(object sender, EventArgs e)
+        {
+            int totalRecord = 0;
+            using (TestBravoEntities db = new TestBravoEntities())
+            {
+                totalRecord = db.tbl_Customer.Count();
+            };
+            if (pageNumber - 1 > 0)
+            {
+                pageNumber--;
+                await LoadDataCustomer(pageNumber, rowOfPage);
+                lbPageNumber.Text = string.Format("Page {0}/{1}", pageNumber, totalRecord / rowOfPage);
+            }
+            
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+             await LoadDataCustomer(pageNumber, rowOfPage);
+        }
+    }
+}
